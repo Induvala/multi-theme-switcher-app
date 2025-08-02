3e0d35e7a137b948cc3f2479d8ab27fda7618f72
Indu <induvala305@gmail.com>
2025-08-02 19:51:48 +0530

    home page by default active

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import { Home } from "./pages/Home";
 import Layout from "./Layout/Layout";
+import { useTheme } from "./context/ThemeContext";
 
 function App() {
+  const {theme} = useTheme();
   return (
     <>
        <Router>       
          <Routes>
           <Route path='/' element={<Layout/>}>
+          {theme === 'theme-2' &&
+          <Route index element={<Navigate to="/home" replace />} />}
           <Route path='/home' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
